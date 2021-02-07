2edb6e9206d7351630f62839b67d1d61a5c56327
Anilton Araujo <aniltondesenv@gmail.com>
2021-02-07 09:09:49 -0300

    primeiro commit, projeto criado

---
+++ src/main/java/com/araujo/estudomongo/EstudoMongoApplication.java
@@ -0,0 +1,13 @@
+package com.araujo.estudomongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstudoMongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstudoMongoApplication.class, args);
+	}
+
+}
