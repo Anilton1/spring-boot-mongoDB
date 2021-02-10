13b6d827c87dbd9de632954ff3cb935d542b05b8
Anilton Araujo <aniltondesenv@gmail.com>
2021-02-10 19:03:46 -0300

    obtendo usuario por id

--- src/main/java/com/araujo/estudomongo/services/UserService.java
+++ src/main/java/com/araujo/estudomongo/services/UserService.java
@@ -1,21 +1,28 @@
 package com.araujo.estudomongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.araujo.estudomongo.domain.User;
 import com.araujo.estudomongo.repository.UserRepository;
+import com.araujo.estudomongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
-	
+
 	@Autowired
 	UserRepository rep;
-	
-	public List<User> findAll(){
+
+	public List<User> findAll() {
 		return rep.findAll();
+	}
+
+	public User findById(String id) {
+		Optional<User> obj = rep.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 
 }
