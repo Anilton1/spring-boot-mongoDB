dfe5b684f0fad43786f29f320d73622207c392d0
Anilton Araujo <aniltondesenv@gmail.com>
2021-02-13 15:20:17 -0300

    obtendo um post por id

---
+++ src/main/java/com/araujo/estudomongo/services/PostService.java
@@ -0,0 +1,22 @@
+package com.araujo.estudomongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.araujo.estudomongo.domain.Post;
+import com.araujo.estudomongo.repository.PostRepository;
+import com.araujo.estudomongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	PostRepository rep;
+
+	public Post findById(String id) {
+		Optional<Post> obj = rep.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+}
