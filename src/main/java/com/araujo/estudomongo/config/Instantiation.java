520ccbd73c46651d570ae6bfb3817e3cc83f8348
Anilton Araujo <aniltondesenv@gmail.com>
2021-02-13 15:33:17 -0300

    acrescentando commentarios aos posts

--- src/main/java/com/araujo/estudomongo/config/Instantiation.java
+++ src/main/java/com/araujo/estudomongo/config/Instantiation.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import com.araujo.estudomongo.domain.Post;
 import com.araujo.estudomongo.domain.User;
 import com.araujo.estudomongo.dto.AuthorDTO;
+import com.araujo.estudomongo.dto.CommentDTO;
 import com.araujo.estudomongo.repository.PostRepository;
 import com.araujo.estudomongo.repository.UserRepository;
 
@@ -36,11 +37,21 @@ public class Instantiation implements CommandLineRunner{
 		
 		Post post1 = new Post(null,sdf.parse("21/03/2021"), "Partiu viagem", "Viajando para Paraíba.", new AuthorDTO(maria));
 		Post post2 = new Post(null,sdf.parse("23/03/2021"), "Bom dia", "BD", new AuthorDTO(maria));		
+		
+		CommentDTO c1 = new CommentDTO("Boa viagem Mary Jane!", sdf.parse("21/03/2021"), new AuthorDTO(alex));
+		CommentDTO c2 = new CommentDTO("Aproveite!", sdf.parse("21/03/2021"), new AuthorDTO(bob));
+		CommentDTO c3 = new CommentDTO("Ótimo dia!", sdf.parse("21/03/2021"), new AuthorDTO(alex));
+		
+		post1.getComments().addAll(Arrays.asList(c1, c2));
+		post2.getComments().addAll(Arrays.asList(c3));
+		
 		repPost.deleteAll();
 		repPost.saveAll(Arrays.asList(post1, post2));
 		
 		maria.setPosts(Arrays.asList(post1, post2));	
 		repUser.save(maria);
+		
+		
 	}
 
 }
--- src/main/java/com/araujo/estudomongo/domain/Post.java
+++ src/main/java/com/araujo/estudomongo/domain/Post.java
@@ -1,12 +1,15 @@
 package com.araujo.estudomongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.araujo.estudomongo.dto.AuthorDTO;
+import com.araujo.estudomongo.dto.CommentDTO;
 
 @Document(collection = "post")
 public class Post implements Serializable {
@@ -21,6 +24,7 @@ public class Post implements Serializable {
 	private String title;
 	private String body;
 	private AuthorDTO author;
+	private List<CommentDTO> comments = new ArrayList<CommentDTO>();
 
 	public Post() {
 	}
@@ -72,6 +76,14 @@ public class Post implements Serializable {
 
 	public void setAuthor(AuthorDTO author) {
 		this.author = author;
+	}
+
+	public List<CommentDTO> getComments() {
+		return comments;
+	}
+
+	public void setComments(List<CommentDTO> comments) {
+		this.comments = comments;
 	}
 
 	@Override
